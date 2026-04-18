346803e4a0196f55e58787b9ceb78b77694f931f
paavana-410 <r07205805@gmail.com>
2026-04-18 21:25:14 +0530

    fix: replace external Google Fonts API with local fontsource package to resolve unresolvable resource integrity hotspot

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/inter/800.css';
 import './index.css';
 
 ReactDOM.render(
